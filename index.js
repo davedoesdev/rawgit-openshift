753392001868a58332db004cff95bd30a129edc6
David Halls <dahalls@gmail.com>
2016-09-08 06:58:48 +0100

    Add openshift adaptations back in

--- index.js
+++ index.js
@@ -97,7 +97,8 @@ app.use((err, req, res, next) => {
 
 // -- Server -------------------------------------------------------------------
 if (app.get('env') !== 'test') {
-  const port = process.env.PORT || 5000;
+  const ip = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
+  const port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
 
   app.listen(port, () => {
     console.log('Listening on port ' + port);
--- server.js
+++ server.js
@@ -1,1 +1,1 @@
-web.js
+index.js
