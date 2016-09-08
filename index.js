19346b01817f78972816cdb1d3b5adf86c7762e8
David Halls <dahalls@gmail.com>
2016-09-08 09:48:29 +0100

    Fix startup

--- index.js
+++ index.js
@@ -100,7 +100,7 @@ if (app.get('env') !== 'test') {
   const ip = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
   const port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
 
-  app.listen(port, () => {
+  app.listen(port, ip, () => {
     console.log('Listening on port ' + port);
   });
 }
